a8d97937b6e7f553139f260536b814a47cb6efd2
Aadesh <aadesh.shigavan01@gmail.com>
2020-07-29 20:21:22 +0530

    Daily Flash

---
+++ 29 july 2020/Program2.java
@@ -0,0 +1,14 @@
+class UpperLower {
+
+	public static void main(String[] args) {
+		
+		char character = 'A';
+
+		if (character == 'A') {
+			System.out.println("a");
+		}
+		else{
+			System.out.println("A");
+		}
+	}
+}
---
+++ 29 july 2020/Program3.java
@@ -0,0 +1,23 @@
+class GreatestNumb {
+
+	public static void main(String[] args) {
+		
+		int num1 = 40;
+
+		int num2 = 70;
+
+		int num3 = 30;
+
+		if (num1 > num2  && num1 > num3) {
+			System.out.println("Largest number is " + num1);
+		}
+
+		else if (num2 > num1  && num2 > num3) {
+			System.out.println("Largest number is " + num2);
+		}
+		else if (num3 > num1  && num3 > num2) {
+			System.out.println("Largest number is " + num3);
+		}
+
+	}
+}
